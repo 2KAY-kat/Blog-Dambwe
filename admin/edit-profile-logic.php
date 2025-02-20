ae7722b8bcd691e44b889beb34cfeb6bcda90bfd
2KAY-kat <dambwedesigns@gmail.com>
2025-02-20 09:06:31 +0200

    edited the profile editing page and previe [pages

--- admin/edit-profile-logic.php
+++ admin/edit-profile-logic.php
@@ -1,71 +1,88 @@
 <?php
-require 'config/database.php';
+require '../config/database.php';
 
 if(isset($_POST['submit'])) {
     $id = $_SESSION['user-id'];
     $firstname = filter_var($_POST['firstname'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
     $lastname = filter_var($_POST['lastname'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
     $bio = filter_var($_POST['bio'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-    $avatar = $_FILES['avatar'];
-    $cover_photo = $_FILES['cover_photo'];
 
     // Validate inputs
     if(!$firstname || !$lastname) {
         $_SESSION['edit-profile'] = "Please enter both first and last name";
     } else {
-        // Process avatar
-        if($avatar['name']) {
+        // Get current user data for existing avatar/cover
+        $current_user_query = "SELECT avatar, cover_photo FROM users WHERE id=$id";
+        $current_user_result = mysqli_query($connection, $current_user_query);
+        $current_user = mysqli_fetch_assoc($current_user_result);
+
+        $avatar_name = $current_user['avatar'];
+        $cover_photo_name = $current_user['cover_photo'];
+
+        // Handle new avatar upload
+        if($_FILES['avatar']['name']) {
+            $avatar = $_FILES['avatar'];
             $time = time();
             $avatar_name = $time . $avatar['name'];
-            $avatar_tmp_name = $avatar['tmp_name'];
-            $avatar_destination_path = '../images/' . $avatar_name;
+            $avatar_tmp = $avatar['tmp_name'];
+            $avatar_path = '../images/' . $avatar_name;
 
-            $allowed_files = ['png', 'jpg', 'jpeg'];
-            $extension = explode('.', $avatar_name);
-            $extension = end($extension);
-            if(in_array($extension, $allowed_files)) {
+            // Validate avatar
+            $allowed = ['png', 'jpg', 'jpeg'];
+            $ext = explode('.', $avatar_name);
+            $ext = end($ext);
+            if(in_array(strtolower($ext), $allowed)) {
                 if($avatar['size'] < 2000000) {
-                    move_uploaded_file($avatar_tmp_name, $avatar_destination_path);
+                    move_uploaded_file($avatar_tmp, $avatar_path);
                 } else {
-                    $_SESSION['edit-profile'] = "File size too big. Should be less than 2MB";
+                    $_SESSION['edit-profile'] = "Avatar file too large. Maximum size is 2MB";
                 }
             } else {
-                $_SESSION['edit-profile'] = "File should be png, jpg, or jpeg";
+                $_SESSION['edit-profile'] = "Avatar must be PNG, JPG, or JPEG";
             }
         }
 
-        // Process cover photo
-        if($cover_photo['name']) {
+        // Handle new cover photo upload
+        if($_FILES['cover_photo']['name']) {
+            $cover = $_FILES['cover_photo'];
             $time = time();
-            $cover_photo_name = 'cover_' . $time . $cover_photo['name'];
-            $cover_photo_tmp_name = $cover_photo['tmp_name'];
-            $cover_photo_destination_path = '../images/' . $cover_photo_name;
+            $cover_photo_name = 'cover_' . $time . $cover['name'];
+            $cover_tmp = $cover['tmp_name'];
+            $cover_path = '../images/' . $cover_photo_name;
 
-            $allowed_files = ['png', 'jpg', 'jpeg'];
-            $extension = explode('.', $cover_photo_name);
-            $extension = end($extension);
-            if(in_array($extension, $allowed_files)) {
-                if($cover_photo['size'] < 2000000) {
-                    move_uploaded_file($cover_photo_tmp_name, $cover_photo_destination_path);
+            // Validate cover photo
+            $allowed = ['png', 'jpg', 'jpeg'];
+            $ext = explode('.', $cover_photo_name);
+            $ext = end($ext);
+            if(in_array(strtolower($ext), $allowed)) {
+                if($cover['size'] < 2000000) {
+                    move_uploaded_file($cover_tmp, $cover_path);
                 } else {
-                    $_SESSION['edit-profile'] = "Cover photo file size too big. Should be less than 2MB";
+                    $_SESSION['edit-profile'] = "Cover photo file too large. Maximum size is 2MB";
                 }
             } else {
-                $_SESSION['edit-profile'] = "Cover photo file should be png, jpg, or jpeg";
+                $_SESSION['edit-profile'] = "Cover photo must be PNG, JPG, or JPEG";
             }
         }
 
         if(!isset($_SESSION['edit-profile'])) {
-            $avatar_to_insert = $avatar_name ?? $avatar['avatar'];
-            $cover_photo_to_insert = $cover_photo_name ?? $user['cover_photo'];
-
-            // Update user
-            $query = "UPDATE users SET firstname=?, lastname=?, bio=?, avatar=?, cover_photo=? WHERE id=?";
-            $stmt = mysqli_prepare($connection, $query);
-            mysqli_stmt_bind_param($stmt, 'sssssi', $firstname, $lastname, $bio, $avatar_to_insert, $cover_photo_to_insert, $id);
+            // Update user in database
+            $update_query = "UPDATE users SET firstname=?, lastname=?, bio=?, avatar=?, cover_photo=? WHERE id=?";
+            $stmt = mysqli_prepare($connection, $update_query);
+            mysqli_stmt_bind_param($stmt, "sssssi", $firstname, $lastname, $bio, $avatar_name, $cover_photo_name, $id);
             
             if(mysqli_stmt_execute($stmt)) {
                 $_SESSION['edit-profile-success'] = "Profile updated successfully";
+                
+                // Update session data
+                $_SESSION['user-data'] = [
+                    'id' => $id,
+                    'firstname' => $firstname,
+                    'lastname' => $lastname,
+                    'avatar' => $avatar_name
+                ];
+            } else {
+                $_SESSION['edit-profile'] = "Error updating profile";
             }
         }
     }
--- admin/edit-user.php
+++ admin/edit-user.php
@@ -1,41 +1,91 @@
 <?php
 include 'partials/header.php';
 
-if(isset($_GET['id'])) {
+if (isset($_GET['id'])) {
     $id = filter_var($_GET['id'], FILTER_SANITIZE_NUMBER_INT);
     $query = "SELECT * FROM users WHERE id=$id";
     $result = mysqli_query($connection, $query);
     $user = mysqli_fetch_assoc($result);
-} else  {
+} else {
     header('location: ' . ROOT_URL . 'admin/manage-users.php');
     die();
 }
-
 ?>
 
+<section class="dashboard">
+    <div class="container dashboard__container">
+        <button id="show__sidebar-btn" class="sidebar__toggle"><i class="fas fa-chevron-right"></i></button>
+        <button id="hide__sidebar-btn" class="sidebar__toggle"><i class="fas fa-chevron-left"></i></button>
 
-<section class="form__section">
-    <div class="container form__section-container">
-        <h2>Edit User</h2>
-       
-        <form action="<?= ROOT_URL ?>admin/edit-user-logic.php" method="post">
-            <input type="hidden" value="<?= $user['id'] ?>" name="id">
-            <input type="text" value="<?= $user['firstname'] ?>" name="firstname" placeholder="First Name">
-            <input type="text" value="<?= $user['lastname'] ?>" name="lastname" placeholder="Last Name">
-            <select name="userrole">
-                <option value="0">Author</option>
-                <option value="1">Admin</option>
-            </select>
+        <aside>
+            <ul>
+                <li><a href="add-post.php"><i class="fas fa-pen"></i>
+                    <h5>Add Post</h5>
+                </a></li>
+                <li><a href="index.php"><i class="fas fa-pencil"></i>
+                    <h5>Manage Posts</h5>
+                </a></li>
+                <?php if(isset($_SESSION['user_is_admin'])): ?>
+                    <li><a href="add-user.php"><i class="fas fa-user-plus"></i>
+                        <h5>Add User</h5>
+                    </a></li>
+                    <li><a href="manage-users.php" class="active"><i class="fas fa-user-cog"></i>
+                        <h5>Manage Users</h5>
+                    </a></li>
+                    <li><a href="add-category.php"><i class="fas fa-edit"></i>
+                        <h5>Add Category</h5>
+                    </a></li>
+                    <li><a href="manage-categories.php"><i class="fas fa-list"></i>
+                        <h5>Manage Categories</h5>
+                    </a></li>
+                <?php endif ?>
+            </ul>
+        </aside>
 
-            <?php if($_SESSION['user_is_admin']): ?>
-                <div class="form__control inline">
-                    <input type="checkbox" name="is_admin" id="is_admin" value="1" <?= $user['is_admin'] ? 'checked' : '' ?>>
-                    <label for="is_admin">Make Admin</label>
+        <main>
+            <h2>Edit User</h2>
+            <?php if(isset($_SESSION['edit-user'])): ?>
+                <div class="alert__message error">
+                    <p>
+                        <?= $_SESSION['edit-user'];
+                        unset($_SESSION['edit-user']);
+                        ?>
+                    </p>
                 </div>
             <?php endif ?>
 
-            <button class="btn" name="submit" type="submit">Update User</button>
-        </form>
+            <form class="form" action="<?= ROOT_URL ?>admin/edit-user-logic.php" method="POST">
+                <input type="hidden" name="id" value="<?= $user['id'] ?>">
+                <div class="form__control">
+                    <label for="firstname">First Name</label>
+                    <input type="text" id="firstname" name="firstname" value="<?= $user['firstname'] ?>">
+                </div>
+                <div class="form__control">
+                    <label for="lastname">Last Name</label>
+                    <input type="text" id="lastname" name="lastname" value="<?= $user['lastname'] ?>">
+                </div>
+                <?php if(isset($_SESSION['user_is_admin'])): ?>
+                <div class="form__control">
+                    <label>User Role</label>
+                    <div class="radio__group">
+                        <div class="radio__option">
+                            <input type="radio" id="author" name="userrole" value="0" <?= $user['is_admin'] ? '' : 'checked' ?>>
+                            <label for="author">Author</label>
+                        </div>
+                        <div class="radio__option">
+                            <input type="radio" id="admin" name="userrole" value="1" <?= $user['is_admin'] ? 'checked' : '' ?>>
+                            <label for="admin">Admin</label>
+                        </div>
+                    </div>
+                </div>
+                <?php endif ?>
+                <button type="submit" name="submit" class="btn">Update User</button>
+            </form>
+        </main>
     </div>
 </section>
+
+<?php
+include '../partials/footer-auth.php';
+?>
 
