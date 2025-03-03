21c02faacd4ccba1be152a5e417c5e307f515b81
2KAY-kat <dambwedesigns@gmail.com>
2025-03-03 23:46:27 +0200

    Enhance navigation with notifications feature and improve profile dropdown layout just the icon only

--- admin/partials/header.php
+++ admin/partials/header.php
@@ -24,6 +24,10 @@ $_SESSION['user_is_admin'] = $current_user['is_admin'];
     <link rel="stylesheet" href="<?= ROOT_URL ?>css/style.css">
     <link rel="stylesheet" href="<?= ROOT_URL ?>css/fontawesome-free-6.6.0-web/fontawesome-free-6.6.0-web/css/all.css">
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+    <script>
+        const ROOT_URL = '<?= ROOT_URL ?>';
+    </script>
+<script src="<?= ROOT_URL ?>js/notifications.js" defer></script>
 </head>
 <body>
     <nav>
@@ -34,22 +38,37 @@ $_SESSION['user_is_admin'] = $current_user['is_admin'];
                 <li><a href="<?= ROOT_URL ?>about.php">About</a></li>
                 <li><a href="<?= ROOT_URL ?>services.php">Services</a></li>
                 <li><a href="<?= ROOT_URL ?>contact.php">Contact</a></li>
+            </ul>
                 <?php if(isset($_SESSION['user-id'])) : ?>
                 <li class="nav__profile">
                     <div class="avatar">
                         <img src="<?= ROOT_URL . 'images/' . $current_user['avatar'] ?>">
                     </div>
                     <ul>
-                        <li><a href="<?= ROOT_URL ?>admin/index.php">Dashboard</a></li>
-                        <li><a href="<?= ROOT_URL ?>admin/profile.php">View Profile</a></li>
+                    <?php if(strpos($_SERVER['PHP_SELF'], '/admin/') !== false): ?>
+                        <li><a href="<?= ROOT_URL ?>admin/profile.php">My Profile</a></li>
                         <li><a href="<?= ROOT_URL ?>logout.php">Logout</a></li>
+                        <?php else: ?>
+                            <li><a href="<?= ROOT_URL ?>admin/index.php">Dashboard</a></li>
+                            <li><a href="<?= ROOT_URL ?>admin/profile.php">Viw Profile</a></li>
+                            <li><a href="<?= ROOT_URL ?>logout.php">Logout</a></li>
                         <?php endif; ?>
                     </ul>
                 </li>
             </ul>
+
+            <?php endif; ?>
+        
+        <div class="nav__buttons">
+            <?php if(isset($_SESSION['user-id'])) : ?>
+                <div class="notifications-icon">
+                    <i class="fa-regular fa-bell"></i>
+                    <span class="notification-count"></span>
+                </div>
+            <?php endif; ?>
             <button id="open__nav-btn"><i class="fas fa-bars"></i></button>
             <button id="close__nav-btn"><i class="fas fa-times"></i></button>
-
+        </div>
         </div>
     </nav>
 
--- partials/header.php
+++ partials/header.php
@@ -1,5 +1,4 @@
 <?php
-// Remove session check since it's now handled in database.php
 require_once __DIR__ . '/../config/database.php';
 
 // current user in db
@@ -33,46 +32,49 @@ if(isset($_SESSION['user-id'])) {
     <script>
         const ROOT_URL = '<?= ROOT_URL ?>';
     </script>
+<script src="<?= ROOT_URL ?>js/notifications.js" defer></script>
 </head>
 <body>
     <?php include 'skeleton-loader.php'; ?>
     <nav>
-        <div class="container nav__container">
-            <a href="<?= ROOT_URL ?>" class="nav__logo"><img src="<?= ROOT_URL ?>partials/favicon.ico" alt=""></a>
-            <ul class="nav__items">
-                <li><a href="<?= ROOT_URL ?>blog.php">Blog</a></li>
-                <li><a href="<?= ROOT_URL ?>about.php">About</a></li>
-                <li><a href="<?= ROOT_URL ?>services.php">Services</a></li>
-                <li><a href="<?= ROOT_URL ?>contact.php">Contact</a></li>
-                <?php if(isset($_SESSION['user-id'])) : ?>
-                    <li class="nav__profile">
+    <div class="container nav__container">
+        <a href="<?= ROOT_URL ?>" class="nav__logo"><img src="<?= ROOT_URL ?>partials/favicon.ico" alt=""></a>
+        <ul class="nav__items">
+            <li><a href="<?= ROOT_URL ?>blog.php">Blog</a></li>
+            <li><a href="<?= ROOT_URL ?>about.php">About</a></li>
+            <li><a href="<?= ROOT_URL ?>services.php">Services</a></li>
+            <li><a href="<?= ROOT_URL ?>contact.php">Contact</a></li>
+            </ul>
+            <?php if(isset($_SESSION['user-id'])) : ?>
+                <li class="nav__profile">
                     <div class="avatar">
                         <img src="<?= ROOT_URL . 'images/' . ($avatar['avatar'] ?: 'default-avatar.png') ?>">
                     </div>
                     <ul>
                         <?php if(strpos($_SERVER['PHP_SELF'], '/admin/') !== false): ?>
-                            <!-- Admin section dropdown -->
                             <li><a href="<?= ROOT_URL ?>admin/profile.php">My Profile</a></li>
                             <li><a href="<?= ROOT_URL ?>logout.php">Logout</a></li>
                         <?php else: ?>
-                            <!-- Main site dropdown -->
                             <li><a href="<?= ROOT_URL ?>admin/index.php">Dashboard</a></li>
                             <li><a href="<?= ROOT_URL ?>admin/profile.php">View Profile</a></li>
                             <li><a href="<?= ROOT_URL ?>logout.php">Logout</a></li>
                         <?php endif; ?>
                     </ul>
                 </li>
-                <?php else : ?>
-                <li><a class="signin-btn" href="<?= ROOT_URL ?>signin.php"><i class="fa fa-sign-in"></i> login</a></li>
-                <li><a class="sign-up-btn" href="<?= ROOT_URL ?>signup.php"><i class="fa fa-sign-in"></i> Signup</a></li>
-                <?php endif ?>
-            </ul>
-
+            <?php endif; ?>
+        
+        <div class="nav__buttons">
+            <?php if(isset($_SESSION['user-id'])) : ?>
+                <div class="notifications-icon">
+                    <i class="fa-regular fa-bell"></i>
+                    <span class="notification-count"></span>
+                </div>
+            <?php endif; ?>
             <button id="open__nav-btn"><i class="fas fa-bars"></i></button>
             <button id="close__nav-btn"><i class="fas fa-times"></i></button>
-
         </div>
-    </nav>
+    </div>
+</nav>
     <?php 
     if (!strpos($_SERVER['REQUEST_URI'], 'signin.php') && 
         !strpos($_SERVER['REQUEST_URI'], 'signup.php')) {
